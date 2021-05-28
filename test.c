cc116b1f80a25e38a5dd1ebb6bcecb391f203449
Marlon-Lindao-ZX <melindao@espol.edu.ec>
2021-05-28 12:49:19 -0500

    2 push

--- test.c
+++ test.c
@@ -1,6 +1,9 @@
 
 #include <stdio.h>
 
+#define MAX 250
+#define HOLA "HOLA MUNDO\n"
+
 int main(){
     int x = 45;
 
@@ -8,9 +11,19 @@ int main(){
 
     int c = getchar();
 
+    if(c < 97 || c > 120){
+        printf("Caracter no permitido\n");
+        return -1;
+    }
+
     printf("%d\n",c);
 
     putchar(c);
 
     putchar('\n');
+
+    printf("%d\n", MAX);
+    printf("%s\n", HOLA);
+
+    return 0;
 }
